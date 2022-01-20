dc03d962b8b67734193f28fb90b6ca15a05c900c
PAPI131099 <58227775+PAPI131099@users.noreply.github.com>
2022-01-20 23:47:51 +0100

    Creacion del modelo del Crud para realizar las operaciones.

--- app/Crud.php
+++ app/Crud.php
@@ -1,3 +1,11 @@
 <?php
 //Este es nuestro namespace para evitar conflictos a la hora de usar la clase de otras clases
 namespace App;
+//Usemos la clase Illuminate de la clase Model
+class Crud extends Model{
+
+    //Nombre de la tabla 
+    protected $table='user';
+    //Nombre de los atributos
+    protected $contentTable=['Nombre','Apellidos','Correo','Password'];
+}
